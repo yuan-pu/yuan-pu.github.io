356a033c25da22e3011fd6e54254ed67e7537a35
yuan-pu <77847503+yuan-pu@users.noreply.github.com>
2026-01-08 11:03:14 +0000

    Deploying to gh-pages from @ yuan-pu/yuan-pu.github.io@e2a6988373dcade364fa7e14996eccac35d82f55 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "yuan-pu/yuan-pu_alt",
+    "data-repo": "yuan-pu/yuan-pu.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
